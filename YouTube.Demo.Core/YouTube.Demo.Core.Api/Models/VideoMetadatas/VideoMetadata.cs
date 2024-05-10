de7aab207d6c2211325ec630745a3fbb33a3a069
Asadbek Rajabov <154442057+rajabov0011@users.noreply.github.com>
2024-05-10 21:07:23 +0500

    Data: Add VideoMetadata

---
+++ YouTube.Demo.Core/YouTube.Demo.Core.Api/Models/VideoMetadatas/VideoMetadata.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+namespace YouTube.Demo.Core.Api.Models.VideoMetadatas
+{
+    public class VideoMetadata
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BlobPath { get; set; }
+        public string Thubnail { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
