8c364a1fde89cce633e0ab0db1571f94f4c6c47b
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-05-14 18:49:31 +0500

    DATA: Migrate VideoMetadatas

---
+++ YouTube.Demo.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.SqlServer;
+
+namespace YouTube.Demo.Core.Api.Brokers.Storages
+{
+    internal partial class StorageBroker: EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseSqlServer(connectionString);
+           
+        }
+    }
+
+}
