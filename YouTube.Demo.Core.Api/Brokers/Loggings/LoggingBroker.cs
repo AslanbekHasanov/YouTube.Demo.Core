5c00d9bf13363f6a1a443a938363fd54267ef30b
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-05-17 18:43:55 +0500

    BROKERS: Implement Logging Broker

---
+++ YouTube.Demo.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+using Microsoft.Extensions.Logging;
+
+namespace YouTube.Demo.Core.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger)=>
+            this.logger = logger;
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception.Message, exception);
+
+        public void LogError(Exception exception)=>
+            this.logger.LogError(exception.Message,exception);
+    }
+}
