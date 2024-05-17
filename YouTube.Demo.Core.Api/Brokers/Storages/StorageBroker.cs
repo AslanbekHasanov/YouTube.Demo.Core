1119c2cf8fc4e057a8a2aadeaa0998fc9c0423cf
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-05-17 20:13:15 +0500

    BBROKERS: Imlement StorageBroker.VideoMetadata

--- YouTube.Demo.Core.Api/Brokers/Storages/StorageBroker.cs
+++ YouTube.Demo.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -24,9 +24,42 @@ namespace YouTube.Demo.Core.Api.Brokers.Storages
             string connectionString = this.configuration.GetConnectionString("DefaultConnection");
             optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             optionsBuilder.UseSqlServer(connectionString);
-           
         }
-        
+
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private IQueryable<T> SelectAll<T>() where T : class
+        {
+            var broker = new StorageBroker(this.configuration);
+
+            return broker.Set<T>();
+        }
+
+        private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class =>
+            await this.FindAsync<T>(objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Modified;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Deleted;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
     }
 
 }
